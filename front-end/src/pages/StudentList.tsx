a72b3b756e4f3f34cdf9197f1df22329bb258042
AnaLara <analara714@gmail.com>
2025-01-29 13:12:57 -0300

    chore(student-list): estiliza a listagem dos alunos registrados

--- front-end/src/pages/StudentList.tsx
+++ front-end/src/pages/StudentList.tsx
@@ -25,8 +25,8 @@ export const StudentList: React.FC = () => {
   return (
     <div>
       <Header />
-      <div className="w-4/5 flex flex-col justify-center items-center mt-0 mb-0 ml-auto mr-auto">
-        <div className="flex flex-row w-full justify-between items-center">
+      <div className="w-4/5 flex flex-col justify-center items-center mt-0 mb-0 ml-auto mr-auto font-poppins">
+        <div className="flex flex-row w-full justify-between items-center mt-10">
           <h2 className="font-extrabold text-2xl">Alunos</h2>
           <Button
             txtButton="Criar Registro"
@@ -35,22 +35,44 @@ export const StudentList: React.FC = () => {
           />
         </div>
         {/* <div> */}
-        <ul className="flex flex-col justify-center items-center w-full">
+        <ul className="flex flex-col justify-center items-stretch w-full mt-0 mb-0 ml-auto mr-auto">
           {students.length === 0 ? (
-            <span className="mt-[30%]">
+            <span className="text-center mt-[30%]">
               Ainda não há estudantes cadastrados
             </span>
           ) : (
-            students.map((student) => (
-              <li key={student._id}>
-                <span> {student.name} </span>
-                <span> {student.age}</span>
-                <span> {student.studentClass}</span>
-                <button onClick={() => ButtonDeleteClickHandler(student._id)}>
-                  deletar
-                </button>
-              </li>
-            ))
+            <div className="flex flex-col w-full mt-10 justify-around">
+              <div className="grid grid-cols-4 items-center justify-center bg-[#EEEEEE] w-full rounded-lg h-12 text-center gap-16">
+                <span>nome</span>
+                <span>idade</span>
+                <span>turma</span>
+                <span>deletar</span>
+              </div>
+              {students.map((student) => (
+                <li
+                  key={student._id}
+                  className="grid grid-cols-4 items-center justify-center text-center w-full gap-16 h-12 mt-4"
+                >
+                  <span> {student.name} </span>
+                  <span> {student.age}</span>
+                  <span> {student.studentClass}</span>
+                  <button
+                    onClick={() => ButtonDeleteClickHandler(student._id)}
+                    className="text-center pl-[50%] cursor-pointer"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      height="24px"
+                      viewBox="0 -960 960 960"
+                      width="24px"
+                      fill="undefined"
+                    >
+                      <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
+                    </svg>
+                  </button>
+                </li>
+              ))}
+            </div>
           )}
         </ul>
         {/* </div> */}
